c57d91a57714f3d5802ee9cf76cd936024de510a
Gavin Mulligan <d3r1v3d@gmail.com>
2010-12-22 22:45:46 -0700

    Now fully implemented.

--- lib/pickem_predictor.rb
+++ lib/pickem_predictor.rb
@@ -17,9 +17,11 @@ module Pickem
             links = entry.search('a')
 
             players << {
-                :url  => root_url + links[0].attributes['href'].text,
-                :name => links[1].text,
-                :picks => []
+                :url            => root_url + links[0].attributes['href'].text,
+                :name           => links[1].text,
+                :current_score  => 0,
+                :max_score_left => 0,
+                :picks          => []
             }
         end
 
@@ -38,24 +40,24 @@ module Pickem
                     ['empty', 'entryPick', 'locked'].include?(v)
                 end
                 player[:picks] << {
-                    :name         => cells[0].text,
-                    :when         => cells[4].text,
-                    :home         => cells[6].search('a').text,
-                    :visitor      => cells[2].search('a').text,
-                    :confidence   => cells[7].search('.confidencePoints .value').text,
-                    :guess        => visitor_classes.include?('empty') ? :home : :visitor,
-                    :guess_status => (visitor_status.empty?) ? home_status.first : visitor_status.first
+                    :name          => cells[0].text,
+                    :when          => cells[4].text,
+                    :home          => cells[6].search('a').text,
+                    :visitor       => cells[2].search('a').text,
+                    :confidence    => cells[7].search('.confidencePoints .value').text,
+                    :guess         => visitor_classes.include?('empty') ? :home : :visitor,
+                    :guess_status  => (visitor_status.empty?) ? home_status.first : visitor_status.first
                 }
             end
         end
 
-        puts "Competitors:"
+        puts "Entries:"
         players.each_with_index do |player, index|
             puts "[#{index}] #{player[:name]}"
         end
 
-        print "Select a player to compare others against [0-#{players.count - 1}]: "
-        selection = gets
+        print "Please select your entry [0-#{players.count - 1}]: "
+        selection = gets.gsub(/\s/, '')
 
         valid_integer = (true if Integer(selection) rescue false)
         if not valid_integer
@@ -72,20 +74,55 @@ module Pickem
 
         players.unshift(players.delete_at(selection))
 
-        puts players.inject('') { |result, player| result + player[:name].ljust(COLUMN_LENGTH) }
-        puts '-' * players.count * COLUMN_LENGTH
-        
+        num_columns     = players.count + 2
         max_bowl_length = players.first[:picks].inject('') do |result, pick|
-            # !!!
-        end
+            result = ((pick[:name] + " Bowl").size > result.size) ? pick[:name] + " Bowl" : result
+        end.size + 1
+        line_break      = '-' * ((num_columns - 1) * COLUMN_LENGTH + max_bowl_length)
 
-        (0...players.first[:picks].count).each do |bowl_index|
-            row = 
+        puts
+        print 'Bowl Name'.ljust(max_bowl_length)
+        puts self.format(
+            players.map{ |player| player[:name] } + ['Your Pick']
+        )
+        puts line_break
+        (0...players.first[:picks].count).each do |pick_index|
+            fulcrum_guess = players.first[:picks][pick_index][:guess]
 
-            (0...players.count).each do |player_index|
-                
-            end
+            print (players.first[:picks][pick_index][:name] + " Bowl").ljust(max_bowl_length)
+            puts self.format(
+                players.map{ |player|
+                    pick = player[:picks][pick_index]
+                    case pick[:guess_status] when 'correct'
+                        player[:current_score] += pick[:confidence].to_i
+
+                        "+#{pick[:confidence]}"
+                    when 'wrong-pick'
+                        "0"
+                    else
+                        guess_confidence = (pick[:guess] == fulcrum_guess) ? "+#{pick[:confidence]}" : "0"
+                        player[:max_score_left] += pick[:confidence].to_i
+
+                        "(#{guess_confidence})"
+                    end 
+                } +
+                [players.first[:picks][pick_index][fulcrum_guess]]
+            )
         end
+        puts line_break
+        print "Current Score".ljust(max_bowl_length)
+        puts self.format(
+            players.map{ |player| player[:current_score].to_s }
+        )
+        print "Max Possible Score".ljust(max_bowl_length)
+        puts self.format(
+            players.map{ |player| (player[:max_score_left] + player[:current_score]).to_s }
+        )
+        puts
+    end
+
+    def self.format(args)
+        args.flatten.inject('') { |result, arg| result + arg.ljust(COLUMN_LENGTH) }
     end
 end
 
